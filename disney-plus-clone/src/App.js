d2116d81aa0432ad6439157b21653b867d63a5c2
Thasanthan-codethamizha <sivakumarthasanthan@gmail.com>
2021-05-02 18:52:10 +0530

    Login Page

--- disney-plus-clone/src/App.js
+++ disney-plus-clone/src/App.js
@@ -1,10 +1,19 @@
-
+import {BrowserRouter as Router,Switch,Route} from 'react-router-dom';
 import "./App.css";
+import Header from './components/Header';
+import Login from './components/Login';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World🚀</h1>
+      <Router>
+        <Header/>
+        <Switch>
+          <Route exact path="/">
+            <Login/>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
